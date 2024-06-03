7dc41f93daff76a8c960a60e8e0f3d353694ab60
Bruno Furtado <bsouzacc@gmail.com>
2024-06-03 02:46:02 -0300

    feature /iniciando as sections de projetos e depoimentos

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const swiperSequencial = new Swiper('.carrousel__depoimentos', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    direction: 'horizontal',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
