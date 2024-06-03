dad2bc1073a2a7f69b191749210082010beb8843
Bruno Furtado <bsouzacc@gmail.com>
2024-06-03 11:29:43 -0300

    feature /depoimentos

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const swiperSequencial = new Swiper('.carrousel__depoimentos', {
     centeredSlides: true,
     spaceBetween: 30,
     loop: true,
+    // autoplay: true,
     direction: 'horizontal',
     pagination: {
         el: ".swiper-pagination",
