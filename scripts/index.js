a2b2668ac4b96ba892889982e97fde902e420fa9
Bruno Furtado <bsouzacc@gmail.com>
2024-06-06 00:37:34 -0300

    feature /construindo menu mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,36 @@
+// Menu Mobile
+const button = document.querySelector('[data-button-menu]');
+const navMenu = document.querySelectorAll('.header');
+const corpoSite = document.querySelector('html')
+
+button.addEventListener('click', function () {
+
+ navMenu.forEach(e => e.classList.toggle("mobile"));
+
+ corpoSite.style = 'overflow: hidden;';
+
+ if(document.querySelector('[data-button-menu]').checked == false){
+  corpoSite.removeAttribute("style");
+ }
+
+  const someBarra = () => {
+    if (window.scrollY >= 500) {
+      navMenu.forEach(e => e.classList.remove("mobile"));
+      corpoSite.removeAttribute("style");
+      document.querySelector('#checkbox-menu').checked = false;
+    }
+  }
+
+  window.addEventListener('scroll', () => {
+    someBarra();
+  });
+
+});
+
+
+
+// slide de depoimentos
+
 const swiperSequencial = new Swiper('.carrousel__depoimentos', {
     slidesPerView: 1,
     centeredSlides: true,
